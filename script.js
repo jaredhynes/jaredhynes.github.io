07bc53487d9560feff55ea5d19cd21c2736aeca5
bdjared <43723086+jaredhynes@users.noreply.github.com>
2024-03-24 21:42:07 -0400

    Fixed bees + information

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function revealFact(insectType) {
   let factText;
   switch (insectType) {
     case "fact1":
-      factText = "Butterflies can taste with their feet!";
+      factText = "In Colonies, bees are divided into three types: Queen, Worker, and Drone. Queens run the hive and produce chemicals to guide the behavior of the other bees. Workers are all female bees and are responsible for food, building and protecting, and circulating air by beating their wings. Drones are the male bees, and their purpose is to mate with the new queen. ";
       break;
     case "fact2":
-      factText = "There are more species of beetles than any other animal group!";
+      factText = "During the summer, the queen bee can lay up to 2,500 eggs per day!";
       break;
     case "fact3":
       factText = "Honeybees can fly up to 15 miles per hour!";
